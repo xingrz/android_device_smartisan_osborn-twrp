6faec5a77885f243179e55dd34445b40f58add33
XiNGRZ <chenxingyu92@gmail.com>
2018-06-25 16:01:21 +0800

    osborn: twrp: Sign images with testkey
    Change-Id: I9e11acbe2ef8acbf2cee6a1a6f2440b4e0b7bbe7

--- omni_osborn.mk
+++ omni_osborn.mk
@@ -33,4 +33,4 @@ PRODUCT_MANUFACTURER := smartisan
 PRODUCT_SUPPORTS_BOOT_SIGNER := true
 PRODUCT_SUPPORTS_VERITY := true
 PRODUCT_SUPPORTS_VERITY_FEC := true
-PRODUCT_VERITY_SIGNING_KEY := build/target/product/security/verity
+PRODUCT_VERITY_SIGNING_KEY := build/target/product/security/testkey
